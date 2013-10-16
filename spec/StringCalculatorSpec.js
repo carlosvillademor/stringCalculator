741466b362c07567ebea2b09fbac32b16bd13707
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2013-10-16 23:15:10 +0100

    refactoring: renaming test methods

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -2,36 +2,36 @@ describe('StringCalculator', function() {
 
     var stringCalculator = new StringCalculator();
 
-    it('should return zero for empty string', function(){
+    it('should return zero when expression is empty string', function(){
         expect(stringCalculator.evaluate('')).toBe(0);
     });
 
-    it('should return 1 if passed the number 1', function() {
+    it('should return zero when expression is null', function(){
+        expect(stringCalculator.evaluate(null)).toBe(0);
+    });
+
+    it('should return number when expression is one operand', function() {
         expect(stringCalculator.evaluate('1')).toBe(1);
     });
 
-    it('should return number if passed a single number with operator', function() {
+    it('should return number when expression is an operand followed by an operator', function() {
         expect(stringCalculator.evaluate('1+')).toBe(1);
         expect(stringCalculator.evaluate('1-')).toBe(1);
     });
 
-    it('should return result for expression', function() {
+    it('should evaluate expression with two operands and an operator', function() {
         expect(stringCalculator.evaluate('1+1')).toBe(2);
     });
 
-    it('should return zero if null is passed in', function(){
-        expect(stringCalculator.evaluate(null)).toBe(0);
-    });
-
-    it('should throw an exception if there is more than one operator of the same type', function(){
+    it('should throw an exception when there are more than one operator of the same type in the expression', function(){
         expect(function(){stringCalculator.evaluate("1+2+3")}).toThrow();
     });
 
-    it('should throw an exception if there is more than type of operator', function(){
+    it('should throw an exception when there are more than one type of operator in the expression', function(){
         expect(function(){stringCalculator.evaluate("1+2-3")}).toThrow();
     });
 
-    it('should throw an exception if there is a non-number operand', function(){
+    it('should throw an exception when there is a non-numeric operand in the expression', function(){
         expect(function(){stringCalculator.evaluate("1a")}).toThrow();
         expect(function(){stringCalculator.evaluate("1A")}).toThrow();
         expect(function(){stringCalculator.evaluate("1+A")}).toThrow();
